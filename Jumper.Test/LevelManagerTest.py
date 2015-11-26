d628bea8a4ff45b11c938e6a7dd0061f63b29087
kechuxiv <girasoltostado@hotmail.com.ar>
2015-11-26 17:09:03 +0000

    Implementacion de MagicMock en LevelManagerTest

--- Jumper.Test/LevelManagerTest.py
+++ Jumper.Test/LevelManagerTest.py
@@ -9,58 +9,94 @@ lib_path = os.path.abspath(os.path.join('..', 'Jumper.UI'))
 sys.path.append(lib_path)
 
 from LevelManager import *
+from mock import MagicMock
 from Point import *
-from PygameImageManager import *
-from PygameSurfaceManager import *
-from PygameSpriteManager import *
+from IImageManager import *
+from ISurfaceManager import *
+from ISpriteManager import *
 
 
 class LevelManagerTest(unittest.TestCase):
 
 	def setUp(self):
-		imageManager = PygameImageManager()
-		surfaceManager = PygameSurfaceManager()
-		spriteManager = PygameSpriteManager()
-		self.levelManager = LevelManager(imageManager, surfaceManager, spriteManager)
+		self.__imageManager__ = IImageManager()
+		self.__surfaceManager__ = ISurfaceManager()
+		self.__spriteManager__ = ISpriteManager()
 
 	def test_GetEnviroment(self):
-		self.levelManager.GetRenderedLevel()
-		enviroment = self.levelManager.GetEnviroment()
+		mockEnviroment = MagicMock()
+		mockEnviroment.Description = "enviroment mockeado"
+		mockSurface = MagicMock()
+		mockSurface.Description = "surface mock"
+		self.__imageManager__.CreateImage = MagicMock(return_value=mockSurface)
+		
+		target = LevelManager(self.__imageManager__, self.__surfaceManager__, self.__spriteManager__)
+		target.enviroment = mockEnviroment
+		enviroment = target.GetEnviroment()
 
 		self.assertIsNotNone(enviroment)
-		self.assertEqual(enviroment.GetStartCords(), Point(1, 4))
-		self.assertEqual(enviroment.GetFinishCords(), Point(18, 4))
-		self.assertTrue(enviroment.IsTile(Point(0,4)))
-		self.assertFalse(enviroment.IsTile(Point(2,4)))
+		self.assertEqual(enviroment.Description, "enviroment mockeado")
 
 	def test_GetEnviroment_ShouldRaiseExceptionIfNotRendered(self):
 		with self.assertRaises(Exception) as cm:
-			enviroment = self.levelManager.GetEnviroment()
+			self.__imageManager__.CreateImage = MagicMock()
+			target = LevelManager(self.__imageManager__, self.__surfaceManager__, self.__spriteManager__)
+			enviroment = target.GetEnviroment()
 
 		self.assertEqual(cm.exception.message, "Level not rendered")
 
 	def test_GetLevel(self):
 		expectedLevel = "leap_of_faith"
-
-		actualLevel = self.levelManager.GetLevel()
+		self.__imageManager__.CreateImage = MagicMock()
+		
+		target = LevelManager(self.__imageManager__, self.__surfaceManager__, self.__spriteManager__)
+		actualLevel = target.GetLevel()
 
 		self.assertEqual(actualLevel, expectedLevel)
 
 	def test_GetLevelPath(self):
 		expectedPath = "../Jumper.Core/Resources/levels/leap_of_faith.png"
-
-		path = self.levelManager.GetLevelPath()
+		self.__imageManager__.CreateImage = MagicMock()
+		
+		target = LevelManager(self.__imageManager__, self.__surfaceManager__, self.__spriteManager__)
+		path = target.GetLevelPath()
 
 		self.assertEqual(path, expectedPath)
 
 	def test_GetRenderedLevel(self):
-		renderedLevel = self.levelManager.GetRenderedLevel()
+		self.__imageManager__.CreateImage = MagicMock()
+		self.__imageManager__.LoadImage = MagicMock()
+		self.__imageManager__.GetImageWidth = MagicMock()
+		self.__imageManager__.GetImageHeight = MagicMock()
+		self.__imageManager__.GetPixelArray = MagicMock()
+		self.__imageManager__.GetImageColor = MagicMock()
+		self.__imageManager__.GetPixelArrayItemColor = MagicMock()
+		self.__surfaceManager__.CreateSurface = MagicMock()
+		self.__surfaceManager__.GetSurface = MagicMock()
+		self.__spriteManager__.CreateSpriteFromSurface = MagicMock()
+		self.__spriteManager__.GetSprite = MagicMock()
+		
+		target = LevelManager(self.__imageManager__, self.__surfaceManager__, self.__spriteManager__)
+		renderedLevel = target.GetRenderedLevel()
 
 		self.assertIsNotNone(renderedLevel)
 
 	def test_GoToNextLevel(self):
-		renderedLevel = self.levelManager.GetRenderedLevel()
-		actualLevel = self.levelManager.GoToNextLevel()
+		self.__imageManager__.CreateImage = MagicMock()
+		self.__imageManager__.LoadImage = MagicMock()
+		self.__imageManager__.GetImageWidth = MagicMock()
+		self.__imageManager__.GetImageHeight = MagicMock()
+		self.__imageManager__.GetPixelArray = MagicMock()
+		self.__imageManager__.GetImageColor = MagicMock()
+		self.__imageManager__.GetPixelArrayItemColor = MagicMock()
+		self.__surfaceManager__.CreateSurface = MagicMock()
+		self.__surfaceManager__.GetSurface = MagicMock()
+		self.__spriteManager__.CreateSpriteFromSurface = MagicMock()
+		self.__spriteManager__.GetSprite = MagicMock()
+		self.__spriteManager__.UpdateSpriteFromSurface = MagicMock()
+	
+		target = LevelManager(self.__imageManager__, self.__surfaceManager__, self.__spriteManager__)
+		actualLevel = target.GoToNextLevel()
 
 		self.assertIsNotNone(actualLevel)
 
